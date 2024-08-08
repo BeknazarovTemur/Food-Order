55d64b64dda9236769f36b410695bea1e956a816
BeknazarovTemur <imtemurb@gmail.com>
2024-08-08 23:29:44 +0500

    first commit

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/admin.css">
+    <title>Food Order Website - Home page</title>
+</head>
+<body>
+ <!-- Menu Section Starts -->
+ <div class="menu text-center">
+     <div class="wrapper">
+         <ul>
+             <li><a href="#">Home</a></li>
+             <li><a href="#">Admin Manager</a></li>
+             <li><a href="#">Category</a></li>
+             <li><a href="#">Food</a></li>
+             <li><a href="#">Order</a></li>
+         </ul>
+     </div>
+ </div>
+ <!-- Menu Section Ends -->
+
+
+ <!-- Main Content Section Starts -->
+ <div class="main-content">
+     <div class="wrapper">
+         <strong>DASHBOARD</strong>
+
+         <div class="col-4">
+             <h1>5</h1>
+             Categories
+         </div>
+     </div>
+ </div>
+ <!-- Main Content Section Ends -->
+
+ <!-- Footer Section Starts -->
+ <div class="footer">
+     <div class="wrapper">
+         <p class="text-center">2024 All rights reserved, Some Restaurant. Developed By - <a href="#">Beknazarov Temur</a></p>
+     </div>
+ </div>
+ <!-- Footer Section Ends -->
+</body>
+</html>
